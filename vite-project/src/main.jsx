c742ee827d6d11be97039a82febffd16578a217c
LAUREN <lv.dean75@gmail.com>
2024-11-16 20:40:57 -0500

    adding pages files. Completed main and app files

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { 
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App.jsx'
+import HomePage from '.pages/HomePage.jsx'
+import About from '.pages/AboutMe.jsx'
+import Contact from '.pages/Contact.jsx'
+import Portfolio from '.pages/Portfolio.jsx'
+import Resume from '.pages/Resume.jsx'
+import Error from '.pages/Error.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Defining the accesible routes and which component to render when the route is accessed
+const router = createBrowserRouter ([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index:true,
+        element: <HomePage />,
+  },
+  { 
+    path: 'AboutMe',
+    element: <About />,
+  },
+  {
+    path: 'Contact',
+    element: <Contact />,
+  },
+  {
+    path: 'Portfolio',
+    element: <Portfolio />,
+  },
+  {
+    path: 'Resume',
+    element: <Resume />,
+  },
+],
+}]);
+
+// Rendering the router component to the root element
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
+
